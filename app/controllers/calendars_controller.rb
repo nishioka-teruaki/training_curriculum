30588015825ee072d751926ce546aa170a3b8db3
nishioka-teruaki <nisioka3@msn.com>
2020-10-07 20:53:28 +0900

    修正３

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,7 +34,11 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan| #mapは要素の数だけ繰り返し結果を配列に返す
         today_plans.push(plan.plan) if plan.date == @todays_date + x #pushは括弧の要素を配列に追加
       end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wdays: (wdays[x])}
+      wday_num = Date.today.wday
+      if wday_num >= 7 #もしもwday_numが7以上であれば、7を引く
+        wday_num = wday_num - 7
+      end
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wdays: (wdays[wday_num])}
       @week_days.push(days) 
     end
 
